8d8fbd5594214574b3561d31af069b74fd56677a
Stéphanie Ramon <stephramon02@gmail.com>
2015-06-02 16:08:28 +0200

    Ajout fichier css, js et menu_circular.php

---
+++ a_livre_ouvert_code/includes/menu_circular.php
@@ -0,0 +1,20 @@
+<div class="component">
+				<!-- Start Nav Structure -->
+				<button class="cn-button" id="cn-button">+</button>
+				<div class="cn-wrapper" id="cn-wrapper">
+				    <ul>
+				      <li><a href="#">Accueil</a></li>
+				      <li><a href="#">Libre de droit</a></li>
+				      <li><a href="#">ePub</a></li>
+				      <li><a href="#">Genres littéraires</a></li>
+				      <li><a href="#">Contexte</a></li>
+				     </ul>
+				</div>
+				<div id="cn-overlay" class="cn-overlay"></div>
+				<!-- End Nav Structure -->
+</div>
+
+		<script src="js/polyfills.js"></script>
+		<script src="js/demo1.js"></script>
+		<!-- For the demo ad only -->   
+<script src="http://tympanus.net/codrops/adpacks/demoad.js"></script>
