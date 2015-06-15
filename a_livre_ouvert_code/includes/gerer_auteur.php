ebab8db7a7f977d030dbcbf7fb5e28caca672a44
Stéphanie Ramon <stephramon02@gmail.com>
2015-06-15 19:16:33 +0200

    Correction gerer_auteur.php
    Fonctionne

--- a_livre_ouvert_code/includes/gerer_auteur.php
+++ a_livre_ouvert_code/includes/gerer_auteur.php
@@ -1,38 +1,32 @@
 <?php
 
+echo "coucou!";
+
 include 'gerer_connexion_db.php';
 
-function listerTousLesAuteurs() {
-	$biblio = openConnextionDb();
-	$auteurs = $biblio->query('select * from auteur order by auteur_nom');
+function listerTousLesAuteurs(){
+    $biblio  = openConnexionDb(); //ouverture de la db;
+    $auteurs = $biblio->query("select * from auteur order by auteur_nom");
 
-	echo "	<table id='table_auteur' class='table_listing'>
-				<thead>
-					<tr>
-						<th>Nom</th>
-						<th>Prenom</th>
-						<th>Naissance</th>
-						<th>Mort</th>
-						<th>Sexe</th>
-					</tr>
-				</thead>
-				<tbody>";
+   echo "<table id='table-auteur' class='table-listing'>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prenom</th>
+                </tr>
+            </thead>
+            <tbody>";
 
-		while ($auteur = $auteurs->fetch()) {
-			echo 	"<tr>" .
-					"<td  class='majuscule'>" . $auteur['auteur_nom'] . "</td>" .
-	                 "<td>" . $auteur['auteur_prenom'] . "</td>" .
-	                 "<td>" . $auteur['auteur_annee_naissance'] . "</td>" .
-	                 "<td>" . $auteur['auteur_annee_deces'] . "</td>" .
-	                 "</tr>";
+        while ($auteur = $auteurs->fetch()){
+            echo "<tr>" .
+                 "<td  class='majuscule'>" . $auteur['auteur_nom'] . "</td>" .
+                 "<td>" . $auteur['auteur_prenom'] . "</td>" .
+                 "</tr>";
         }/*while*/
-    
     echo "</tbody>
           </table>";
-    $biblio = null; // fermeture de la db"
-
-}/*function*/
-
+    $biblio = null; // fermeture de la db
+}
 
 
 ?>
