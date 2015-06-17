52e611105a2da7f297a88a3297dd1129d1d574b4
lauragenot <genot.laura@gmail.com>
2015-06-17 20:45:54 +0200

    modif css
    regarder fichier content.php et menu-horizontale.php

---
+++ a_livre_ouvert_code/content.php
@@ -0,0 +1,3 @@
+<section>
+    <p> Site de littérature française avec textes libres de droit au format epub. </p>
+</section>
---
+++ a_livre_ouvert_code/menu-horizontale.php
@@ -0,0 +1,11 @@
+<section>
+    <header class="menu-horizontale">
+    </header>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Libre de droit</a></li>
+            <li><a href="#">ePub</a></li>
+            <li><a href="#">Genres littéraires</a></li>
+            <li><a href="#">contexte</a></li>
+        </ul>
+</section>
