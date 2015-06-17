29be68f66c80d8c576f97f11c661e3ff6f5ec3ca
Stéphanie Ramon <stephramon02@gmail.com>
2015-06-17 09:42:14 +0200

    Modification left-menu

--- a_livre_ouvert_code/includes/menu_aside.php
+++ a_livre_ouvert_code/includes/menu_aside.php
@@ -1,24 +1,21 @@
 <section>
     <header class="header_menu">
-        <h2>Les livres</h2>
+        <h2>Rechercher</h2>
     </header>
         <ul>
             <li><a href="livre_lister.php">Liste des livres</a></li>
             <li><a href="auteur_lister.php">Liste des auteurs</a></li>
             <li><a href="genre_lister.php">Recherche par genre</a></li>
+            <li><a href="epoque_lister.php">Recherche par période</a></li>
             <li><a href="livre_rechercher.php">Chercher un livre</a></li>
             <li><a href="#">Chercher un auteur</a></li> 
         </ul>
 </section>
 <section>
     <header class="header_menu">
-        <h2>Les auteurs</h2>
+        <h2>Ajouter</h2>
     </header>
         <ul>
             <li><a href="#">Encoder un livre</a></li>
-            <li><a href="#">Modifier un livre</a></li>
-            <li><a href="#">Supprimer un livre</a></li>
             <li><a href="auteur_encoder.php">Encoder un auteur</a></li>
-            <li><a href="#">Modifier un auteur</a></li>
-            <li><a href="#">Supprimer un auteur</a></li>
         </ul>
